80e4f916aea1565995c9d1fcb6fc68f000288f29
Brian\Eric <limike126@gmail.com>
2020-04-25 13:45:04 +0800

    add assets

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('auth/login', { title: 'Express' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('auth/register', { title: 'Express' });
+});
+
 module.exports = router;
